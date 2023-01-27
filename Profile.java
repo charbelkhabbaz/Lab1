e7abb84ba1c9877944873c7de2f120dbe7f60e57
charbelkhabbaz <ckhabbaz7@gmail.com>
2023-01-27 18:05:49 +0200

    Revert "My hobby is added"
    This reverts commit 8867e670318a9304156f6f495ad6e6cb2ff3de16.

--- Profile.java
+++ Profile.java
@@ -3,7 +3,5 @@ public class Profile {
 		System.out.println("a-My name is Charbel khabbaz");
 		System.out.println("b-Computer engineering ");
 		System.out.println("c-1st year");
-		System.out.println("d-Athlete");
-
 	}
 }
