021ad0d45a962c4978337e3524e77eeef46117c0
charbelkhabbaz <ckhabbaz7@gmail.com>
2023-01-27 17:13:53 +0200

    initial info provided

---
+++ Profile.java
@@ -0,0 +1,7 @@
+public class Profile {
+	public static void main(String[] args){
+		System.out.println("a-My name is Charbel khabbaz");
+		System.out.println("b-Computer engineering ");
+		System.out.println("c-1st year");
+	}
+}
